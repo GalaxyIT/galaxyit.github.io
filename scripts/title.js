70b6eb56d6cb009c62ce2bb745f527614ff5762f
Chao Tzu-Hsien <37932792+ZhaoTzuHsien@users.noreply.github.com>
2019-08-10 23:35:21 +0800

    Update title.js

--- scripts/title.js
+++ scripts/title.js
@@ -1,3 +1,10 @@
+function replaceLast(x, y, z){
+  var a = x.split("");
+  a[x.lastIndexOf(y)] = z;
+  return a.join("");
+}
+
 original = document.querySelector('title').textContent
-newStr = original.replace('Jenkins', 'Galaxy IT')
+// newStr = original.replace('Jenkins', 'Galaxy IT')
+newStr = replaceLast(original, 'Jenkins', 'Galaxy IT')
 document.querySelector('title').textContent = newStr
