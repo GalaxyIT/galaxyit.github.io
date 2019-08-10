a06321aaf5cc70619736157a783f0c2af4280ef5
Chao Tzu-Hsien <37932792+ZhaoTzuHsien@users.noreply.github.com>
2019-08-10 23:31:34 +0800

    Update title.js

--- scripts/title.js
+++ scripts/title.js
@@ -1,3 +1,3 @@
 original = document.querySelector('title').textContent
 newStr = original.replace('Jenkins', 'Galaxy IT')
-$('title').text(newStr)
+document.querySelector('title').textContent = newStr
