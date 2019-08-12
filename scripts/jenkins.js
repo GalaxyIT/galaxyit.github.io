077662fa0fbfb352547d05bba9bb9a6aecd78ed8
Chao Tzu-Hsien <37932792+ZhaoTzuHsien@users.noreply.github.com>
2019-08-12 13:18:38 +0800

    Update and rename title.js to jenkins.js

--- scripts/jenkins.js
+++ scripts/jenkins.js
@@ -5,6 +5,12 @@ function reverseString(str) {
     return joinArray
 }
 
+// change title
 original = document.querySelector('title').textContent
 newStr = reverseString(reverseString(original).replace(reverseString('Jenkins'), reverseString('Galaxy IT')))
 document.querySelector('title').textContent = newStr
+
+// add markdown support
+var markdown = document.getElementById('markdown')
+if (markdown)
+    markdown.innerHtml = marked(markdown.textContent)
